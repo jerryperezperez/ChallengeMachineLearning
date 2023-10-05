99552f5b6f3860bcd8d18c8d3234e8d486a60ea4
jerry Perez <jerryperezxbox@gmail.com>
2023-10-05 12:16:28 -0600

    Comentarios agregados

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import requests
 
+# URl definida al iniciar el servicio en modo local
 url = 'http://localhost:5000/procesar'
+
+# Definición del objeto JSON que es enviado como petición al servicio
 datos = {
     "oraciones": [
         "Apple está buscando comprar una startup del Reino Unido por mil millones de dólares.",
@@ -8,7 +11,9 @@ datos = {
     ]
 }
 
+# Se envía por el método POST y se almacena la respuesta en la variable response
 response = requests.post(url, json=datos)
 
+# Se imprime el estado del código y la respuesta generada por el servicio en formato JSON
 print(response.status_code)
 print(response.json())
